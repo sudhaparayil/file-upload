dacdae54cd3ed4834478f79c6f388f658e9250c3
sudhaparayil <parayilsudha@gmail.com>
2018-10-28 13:03:09 +0530

    multer file upload

---
+++ Node.js-uploadFiles/app/controllers/file.controller.js
@@ -0,0 +1,21 @@
+const uploadFolder = __basedir + '/uploads/';
+const fs = require('fs');
+ 
+exports.uploadFile = (req, res) => {
+	res.send('File uploaded successfully! -> filename = ' + req.file.filename);
+}
+ 
+exports.listUrlFiles = (req, res) => {
+	fs.readdir(uploadFolder, (err, files) => {
+		for (let i = 0; i < files.length; ++i) {
+			files[i] = "http://localhost:8080/api/file/" + files[i];
+		}
+		
+		res.send(files);
+	})
+}
+ 
+exports.downloadFile = (req, res) => {
+	let filename = req.params.filename;
+	res.download(uploadFolder + filename);  
+}
